f786909993af7e947d315a4981d4ef56fa564eff
Alex Nguyen <alexvn.edu@gmail.com>
2021-07-24 10:30:16 +0700

    Initial Commit

---
+++ credit-card-ocr-demoTests/credit_card_ocr_demoTests.swift
@@ -0,0 +1,33 @@
+//
+//  credit_card_ocr_demoTests.swift
+//  credit-card-ocr-demoTests
+//
+//  Created by Alex Nguyen on 24/07/2021.
+//
+
+import XCTest
+@testable import credit_card_ocr_demo
+
+class credit_card_ocr_demoTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
